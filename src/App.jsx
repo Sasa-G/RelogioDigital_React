829d3a37a11f58eb2b36228454093bfb6a7dcfa6
Samanta Gomes De Morais Santos <samantagdms@gmail.com>
2024-05-18 21:39:35 -0300

    Primeiro Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import RelogioDigital from './components/RelogioDigital'
+
+function App(){
+  return(
+    
+    <div className= "App">
+    <RelogioDigital/>
+    </div>
+    
+  )
+}
+
+export default App
